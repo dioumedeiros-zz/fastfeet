4e60122d1116ee70aa0a46bf6ace1ecbbe4c12bb
DiouMedeiros <diou.182@gmail.com>
2020-02-20 23:21:39 -0300

    feat: Created recipients and couriers tables

---
+++ server/src/database/migrations/20200221021215-create-couriers.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('couriers', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      avatar_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('couriers');
+  },
+};

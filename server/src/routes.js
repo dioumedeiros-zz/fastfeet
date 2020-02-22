7ed1ca5e1e1bba907711d045ac21699e400a37df
DiouMedeiros <diou.182@gmail.com>
2020-02-22 00:26:23 -0300

    feat: Created middlewares

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,11 +3,14 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.post('/users', UserController.store);
 
 export default routes;

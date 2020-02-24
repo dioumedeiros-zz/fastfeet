eade9cb73af5a9eb45b390d8ea56fddc3e4199e5
DiouMedeiros <diou.182@gmail.com>
2020-02-24 20:09:10 -0300

    fix: Adjust tables

--- server/src/database/migrations/20200224224238-create-orders.js
+++ server/src/database/migrations/20200224224238-create-orders.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: 'couriers',
+          model: 'deliverymans',
           key: 'id',
         },
       },

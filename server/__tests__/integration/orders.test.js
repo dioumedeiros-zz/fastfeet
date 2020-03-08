d81a94ea1b0e2d83fb4a213b425dce68c279f2fd
DiouMedeiros <diou.182@gmail.com>
2020-03-08 19:48:01 -0300

    chore: Updated orders with middlewares

--- server/__tests__/integration/orders.test.js
+++ server/__tests__/integration/orders.test.js
@@ -1,10 +1,32 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import factory from '../factories';
+import truncate from '../util/truncate';
+
 describe('Orders', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
+    await factory.create('User', {
+      email: 'admin@fastfeet.com',
+      password: '123456',
+    });
+
+    const resSession = await request(app)
+      .post('/sessions')
+      .send({
+        email: 'admin@fastfeet.com',
+        password: '123456',
+      });
+
+    console.log(resSession.body);
+
     const response = await request(app)
       .post('/orders')
+      .set('Authorization', `Bearer ${resSession.body.token}`)
       .send({
         product: 'Product test',
         canceled_at: new Date(),

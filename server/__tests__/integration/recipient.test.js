ad71ed598eff2bdc3dd2dc72d73484e00ae86072
DiouMedeiros <diou.182@gmail.com>
2020-03-08 19:48:17 -0300

    chore: Updated recipients with middlewares

--- server/__tests__/integration/recipient.test.js
+++ server/__tests__/integration/recipient.test.js
@@ -1,10 +1,30 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import factory from '../factories';
+import truncate from '../util/truncate';
+
 describe('Recipient', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
+    await factory.create('User', {
+      email: 'admin@fastfeet.com',
+      password: '123456',
+    });
+
+    const resSession = await request(app)
+      .post('/sessions')
+      .send({
+        email: 'admin@fastfeet.com',
+        password: '123456',
+      });
+
     const response = await request(app)
       .post('/recipients')
+      .set('Authorization', `Bearer ${resSession.body.token}`)
       .send({
         name: 'Dionatan Medeiros',
         email: 'diou@gmail.com',

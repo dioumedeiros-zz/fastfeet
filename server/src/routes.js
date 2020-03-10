1609baafd321c8919a17e605da852a188f56d86b
DiouMedeiros <diou.182@gmail.com>
2020-03-09 23:12:58 -0300

    feat: Created User avatar

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 import OrderController from './app/controllers/OrderController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
@@ -17,6 +21,7 @@ routes.post('/users', UserController.store);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.post('/orders', OrderController.store);
 routes.post('/recipients', RecipientController.store);
+routes.post('/files', upload, FileController.store);
 
 routes.get('/deliverymans', DeliverymanController.index);
 

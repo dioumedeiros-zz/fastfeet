55fb85eb8e3fa7820d114ad9e70a88ab814af6b4
DiouMedeiros <diou.182@gmail.com>
2020-03-10 23:23:03 -0300

    feat: Created e-mail config

--- server/src/app/controllers/OrderController.js
+++ server/src/app/controllers/OrderController.js
@@ -1,8 +1,15 @@
 import Order from '../models/Order';
+import Mail from '../../config/mail';
 
 class OrderController {
   async store(req, res) {
     const order = await Order.create(req.body);
+
+    await Mail.sendMail({
+      to: `teste`,
+      subject: 'Encomenta efetuada com sucesso',
+      text: 'Deu boa',
+    });
 
     return res.json(order);
   }
---
+++ server/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'cdf5a31627cf19',
+    pass: '2374bdde56680a',
+  },
+  default: {
+    from: 'Equipe Fastfeet <noreply@fastfeet.com>',
+  },
+};

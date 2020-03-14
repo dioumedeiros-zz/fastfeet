99ce1dd87e2b60eb056f6efd238a41f4b864f3cd
DiouMedeiros <diou.182@gmail.com>
2020-03-14 17:15:07 -0300

    feat: Created jobs controls

--- server/src/app/controllers/OrderController.js
+++ server/src/app/controllers/OrderController.js
@@ -1,23 +1,13 @@
-import { format } from 'date-fns';
-import pt from 'date-fns/locale/pt';
 import Order from '../models/Order';
-import Mail from '../../config/mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class OrderController {
   async store(req, res) {
     const order = await Order.create(req.body);
 
-    await Mail.sendMail({
-      to: `teste`,
-      subject: 'Encomenta efetuada com sucesso',
-      tamplate: 'cancelation',
-      context: {
-        provider: 'aquele',
-        user: 'Dionatan',
-        data: format(new Date(), "'dia' dd 'de' MMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      order,
     });
 
     return res.json(order);
